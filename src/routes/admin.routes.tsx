8829011f968b9b77031083becc6bee96bd1b4659
MostakemHossain <mostakememon123@gmail.com>
2024-04-19 11:26:43 +0600

    sidebar items generator

--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -1,15 +1,8 @@
-import { ReactNode } from "react";
-import { NavLink } from "react-router-dom";
 import AdminDashBoard from "../pages/admin/AdminDashBoard";
 import CreateAdmin from "../pages/admin/CreateAdmin";
 import CreateFaculty from "../pages/admin/CreateFaculty";
 import CreateStudent from "../pages/admin/CreateStudent";
 
-type TSidebarItems = {
-  key: string;
-  label: ReactNode;
-  children?: TSidebarItems[];
-};
 export const adminPaths = [
   {
     name: "Dashboard",
@@ -37,27 +30,3 @@ export const adminPaths = [
     ],
   },
 ];
-
-export const adminSidebarItems = adminPaths.reduce(
-  (acc: TSidebarItems[], item) => {
-    if (item.name && item.path) {
-      acc.push({
-        key: item.name,
-        label: <NavLink to={`/admin/${item.path}`}>{item.name}</NavLink>,
-      });
-    }
-
-    if (item.children) {
-      acc.push({
-        key: item.name,
-        label: item.name,
-        children: item.children.map((child) => ({
-          key: child.name,
-          label: <NavLink to={`/admin/${child.path}`}>{child.name}</NavLink>,
-        })),
-      });
-    }
-    return acc;
-  },
-  []
-);
